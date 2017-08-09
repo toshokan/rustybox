d5cd46cc7a1d33b6b550c9af13727039a36f2e4c
toshokan <toshokan@shojigate.net>
2017-08-08 23:52:36 -0400

    Modified path handling

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,42 @@
 use std::io;
 use std::io::Write;
 use std::env;
-use std::path::Path;
+use std::path::{Path,PathBuf};
 use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    let curr_path = env::current_dir().unwrap();
     let mut env:HashMap<String, String> = HashMap::new();
-    env.insert(String::from("path"), String::from(curr_path.to_str().unwrap()));
     env.insert(String::from("prompt"), String::from("rustybox >"));
 
     loop {
-        print!("{}{} > ", env.get(&String::from("prompt")).unwrap(), env.get(&String::from("path")).unwrap());
+        let curr_path = env::current_dir().unwrap();
+        print!("{}{} > ", env.get(&String::from("prompt")).unwrap(), curr_path.display());
         io::stdout().flush().expect("Failed to flush line");
         let mut userin = String::new();
         io::stdin().read_line(&mut userin).expect("Failed to read line");
-        process_input(&userin, &mut env);
+        process_input(&userin);
     }
 }
 
-fn process_input(ui:&String, e:&mut HashMap<String, String>) {
+fn process_input(ui:&String) {
     let ui_vec:Vec<&str> = ui.split_whitespace().collect();
     match ui_vec[0] {
-        "cd" => cd_main(&ui_vec, e),
+        "cd" => cd_main(&ui_vec),
         "ls" => ls_main(&ui_vec),
         _ => ()
     }
 }
 
-fn cd_main(it:&Vec<&str>, e:&mut HashMap<String, String>){
+fn cd_main(it:&Vec<&str>){
     let home_dir = env::home_dir().unwrap();
+    let mut n_path = PathBuf::new();
     let mut path_to = Path::new(home_dir.to_str().unwrap());
     if it.len() > 1 {
-        path_to = Path::new(it[1]);
+        n_path = parse_path(&String::from(it[1]));
+        path_to = n_path.as_path();
     }
-    match env::set_current_dir(&path_to) {
-        Ok(_) => {
-            let curr_path = env::current_dir().unwrap();
-            e.insert(String::from("path"), String::from(curr_path.to_str().unwrap()));
-        }
-        Err(_) => ()
-    }
+    env::set_current_dir(&path_to).unwrap();
 }
 
 fn ls_main(it:&Vec<&str>){
@@ -58,4 +53,36 @@ fn ls_main(it:&Vec<&str>){
         }
         _ => println!("oop")
     }
+}
+
+fn parse_path(s:&String) -> PathBuf {
+   let mut path = PathBuf::new();
+   if Path::new(s).is_relative() {
+       path = env::current_dir().unwrap();
+   }
+   for v in s.split("/") {
+       match v {
+           "~" => {
+               let home_dir = env::home_dir().unwrap();
+               path.push(home_dir);
+           }
+           ".." => {
+               let mut temp = PathBuf::new();
+               match path.parent() {
+                  Some(p) => {
+                      temp.push(p);
+                  }
+                  None => {
+                      let root_dir = Path::new("/");
+                      temp.push(root_dir);
+                  }
+               }
+               path = temp;
+           }
+           e => {
+               path.push(e);
+           }
+       }
+   }
+   return path;
 }
