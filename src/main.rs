ed6ffefef41c76667e56d35f86b983b276bb8806
toshokan <toshokan@shojigate.net>
2017-08-09 12:21:23 -0400

    Improved path handling and 'ls' functionality. Implemented switch to show hidden files.

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,9 @@ fn main() {
 
 fn process_input(ui:&String) {
     let ui_vec:Vec<&str> = ui.split_whitespace().collect();
+    if ui_vec.len() < 1 {
+        return;
+    }
     match ui_vec[0] {
         "cd" => cd_main(&ui_vec),
         "ls" => ls_main(&ui_vec),
@@ -41,14 +44,40 @@ fn cd_main(it:&Vec<&str>){
 
 fn ls_main(it:&Vec<&str>){
     let current_dir = env::current_dir().unwrap();
+    let mut path_buff = PathBuf::new();
     let mut path_req = Path::new(current_dir.to_str().unwrap());
+    let mut hidden = false;
     if it.len() > 1 {
-        path_req = Path::new(it[1]);
+        match it.binary_search(&"-a") {
+            Ok(index) => {
+                hidden = true;
+                if index == 1 {
+                    path_buff = parse_path(&String::from(it[2]));
+                    path_req = path_buff.as_path(); 
+                } else {
+                    path_buff = parse_path(&String::from(it[1]));
+                    path_req = path_buff.as_path(); 
+                }
+            }
+            Err(_) => {
+                path_buff = parse_path(&String::from(it[1]));
+                path_req = path_buff.as_path(); 
+            }
+        }
     }
     match fs::read_dir(&path_req) {
         Ok(o) => {
             for v in o {
-                println!("{}", v.ok().unwrap().path().display())
+                let v_path = v.ok().unwrap().path();
+                if v_path.file_name().unwrap().to_str().unwrap().chars().next() == Some('.'){
+                    if hidden {
+                        println!("{}", v_path.display());
+                    } else {
+                        continue;
+                    }
+                }
+                //println!("{}", v.ok().unwrap().path().display())
+                println!("{}", v_path.display());
             }
         }
         _ => println!("oop")
